94e9faad31e689e8e31f11f5b9ce378a8f11f9d2
Palash Halder <mpalash.halder@gmail.com>
2017-04-16 14:49:52 +0300

    array used for pallets and WS status
    Busy status to be implemented

--- WS_Agent.js
+++ WS_Agent.js
@@ -1,17 +1,287 @@
-/**
- * Created by halder on 09-Apr-17.
- */
-var WS_Agent = function WS_Agent(name, capability) {
+var WS_Status = ['WS1Stat', 'WS2Stat', 'WS3Stat', 'WS4Stat','WS5Stat', 'WS6Stat','WS7Stat', 'WS8Stat','WS9Stat', 'WS10Stat', 'WS11Stat', 'WS12Stat'];
+var pallets = [{}];
+
+var WS_Agent = function WS_Agent(name, capability, neighbour, port ) {
     this.name_ = name;
     this.capability_ = capability;
+    this.neighbour_ = neighbour;
+    this.port_ = port;
+    this.hostname_ = "localhost";
 };
-
 WS_Agent.prototype.getName = function () {
     return this.name_;
 };
 
 WS_Agent.prototype.getCapability = function () {
     return this.capability_;
+};
+
+WS_Agent.prototype.runServer = function () {
+    var express = require('express');
+    var bodyParser = require('body-parser');
+    var request = require('request');
+
+    var port = this.port_;
+    var hostname = this.hostname_;
+    var WS = this.name_;
+    var WSnum = parseInt(port.toString().substr(2,3));
+
+    var app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    var WS_Neighbour = this.neighbour_;
+
+    function setPallet(pallet) {
+        pallets.push(pallet);
+    }
+    function findPalletinPallets(palletID) {
+        for(var i=0;i<pallets.length;i++){
+            if(pallets[i].palletID_ == palletID)
+            {
+                return i;
+            }
+        }
+    }
+    function resetPallets(palletID) {
+        var index = findPalletinPallets(palletID);
+        pallets[index]="done" + palletID;
+        console.log(pallets);
+    }
+    var currentStatus = "free";
+    function setStatus(stat) {
+        currentStatus = stat;
+    }
+    function getStatus() {
+        return currentStatus;
+    }
+    function palletRequest(url, palletIndex) {
+        request({
+            url: url,
+            method: "POST",
+            body: JSON.stringify(pallets[palletIndex]),
+            headers:{'Content-Type':'application/json'}
+        },function (err, res, body) {});
+    }
+    function simRequest(url) {
+        request({
+            url: url,
+            method: "POST",
+            body: JSON.stringify({destUrl:'http://hostname'}),
+            headers:{'Content-Type':'application/json'}
+        },function (err, res, body) {});
+    }
+
+    app.post('/'+WS+'notifs', function (req, res) {
+        var event = req.body.id;
+        var sender = req.body.senderID;
+        var destination = [];
+        var WS_ID = "WS"+sender.substr(6,2);
+
+        switch (event) {
+            case "Z1_Changed": {
+                if((getStatus() == "free")||(WS_ID=='WS1')){
+                    if (req.body.payload.PalletID != -1) {
+                        console.log(WS,'Z1changed');
+                        //console.log(pallets);
+                        var palletIndex = findPalletinPallets(req.body.payload.PalletID);
+                        console.log('pallet is in: ',palletIndex);
+                        var path = pallets[palletIndex].path_[0];
+                        console.log('path is', path);
+                        if(pallets[palletIndex].path_.length!=0 ){
+                            for(var i=0; i<path.length; i++){
+                                destination[i] = path[i];
+                            }
+                        }
+                        if(WS_ID == 'WS1'){
+                            global.palletIndexWS1 = palletIndex;
+                            pallets[palletIndex].path_.shift();
+                            var url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone12';
+                            simRequest(url);
+                        }
+                        else{
+                            if((destination[0] == WS_ID)||(destination[1] == WS_ID)||(destination[2] == WS_ID)||(destination[3] == WS_ID)){
+                                pallets[palletIndex].path_.shift();
+                                var url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone12';
+                                simRequest(url);
+                            }
+                            else{
+                                url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone14';
+                                simRequest(url);
+                            }
+                        }
+                    }
+                }
+                else{
+                    url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone14';
+                    simRequest(url);
+                }
+                break;
+            }
+            case "Z2_Changed": {
+                if (req.body.payload.PalletID != -1) {
+                    url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone23';
+                    simRequest(url);
+                }
+                break;
+            }
+            case "Z3_Changed": {
+                if (req.body.payload.PalletID != -1) {
+                    palletIndex = findPalletinPallets(req.body.payload.PalletID);
+                    var palletStatus = pallets[palletIndex].status_;
+                    switch (palletStatus){
+                        case 0 : {
+                            url = 'http://localhost:3000/RTU/SimROB1/services/LoadPaper';
+                            simRequest(url);
+                            break;
+                        }
+                        case 1: {
+                            var frameType = pallets[palletIndex].frameType_;
+                            url = 'http://localhost:3000/RTU/SimROB'+WSnum+'/services/Draw'+frameType;
+                            simRequest(url);
+                            break;
+                        }
+                        case 2: {
+                            var screenType = pallets[palletIndex].screenType_;
+                            url = 'http://localhost:3000/RTU/SimROB'+WSnum+'/services/Draw'+screenType;
+                            simRequest(url);
+                            break;
+                        }
+                        case 3: {
+                            var keyType = pallets[palletIndex].keyType_;
+                            url = 'http://localhost:3000/RTU/SimROB'+WSnum+'/services/Draw'+keyType;
+                            simRequest(url);
+                            break;
+                        }
+                        default:{
+                            //palletRequest(WS_Neighbour,currentPallet);
+                            url = 'http://localhost:3000/RTU/SimCNV'+WSnum+'/services/TransZone35';
+                            simRequest(url);
+                        }
+                    }
+                }
+                break;
+            }
+            case "Z4_Changed": {
+                if (req.body.payload.PalletID != -1) {
+                    url = 'http://localhost:3000/RTU/'+sender+'/services/TransZone45';
+                    simRequest(url);
+                }
+                break;
+            }
+            case "Z5_Changed": {
+                if (req.body.payload.PalletID != -1) {
+                    setStatus("free");
+                    palletIndex = findPalletinPallets(req.body.payload.PalletID);
+                    if(WS_ID=='WS6'){
+                        palletRequest(WS_Neighbour, palletIndex);
+                    }
+                }
+                break;
+            }
+            case "PaperLoaded":{
+                console.log('WS-Paperload',palletIndexWS1);
+                pallets[palletIndexWS1].status_++;
+                url = 'http://localhost:3000/RTU/SimCNV'+WSnum+'/services/TransZone35';
+                simRequest(url);
+                break;
+            }
+            case "DrawEndExecution":{
+                var recipe = parseInt(req.body.payload.Recipe);
+                palletIndex = findPalletinPallets(req.body.payload.PalletID);
+                switch (recipe){
+                    case 1:
+                    case 2:
+                    case 3:{
+                        pallets[palletIndex].frameType_ = "done" + recipe;
+                        break;
+                    }
+                    case 4:
+                    case 5:
+                    case 6:{
+                        pallets[palletIndex].screenType_ = "done" + recipe;
+                        break;
+                    }
+                    case 7:
+                    case 8:
+                    case 9:{
+                        pallets[palletIndex].keyType_ = "done" + recipe;
+                        break;
+                    }
+                }
+                pallets[palletIndex].status_=pallets[palletIndex].status_+1;
+                path = pallets[palletIndex].path_[0];
+                if(pallets[palletIndex].path_.length!=0 ){
+                    for(i=0; i<path.length; i++){
+                        destination[i] = path[i];
+                    }
+                }
+                if((destination[0] == WS_ID)||(destination[1] == WS_ID)||(destination[2] == WS_ID)||(destination[3] == WS_ID)){
+                    pallets[palletIndex].path_.shift();
+                    if(pallets[palletIndex].status_==2){
+                        screenType = pallets[palletIndex].screenType_;
+                        url = 'http://localhost:3000/RTU/SimROB'+WSnum+'/services/Draw'+screenType;
+                        simRequest(url);
+                    }
+                    if(pallets[palletIndex].status_==3){
+                        keyType = pallets[palletIndex].keyType_;
+                        url = 'http://localhost:3000/RTU/SimROB'+WSnum+'/services/Draw'+keyType;
+                        simRequest(url);
+                    }
+                    var screenStat = pallets[palletIndex].screenType_.toString();
+                    var keyStat = pallets[palletIndex].keyType_.toString();
+                    if((screenStat.substr(0,4) == "done")&&((keyStat).substr(0,4) == "done")||(keyStat).substr(0,4) == "done"){
+                        url = 'http://localhost:3000/RTU/SimCNV'+WSnum+'/services/TransZone35';
+                        simRequest(url);
+                    }
+                }
+                else{
+                    url = 'http://localhost:3000/RTU/SimCNV'+WSnum+'/services/TransZone35';
+                    simRequest(url);
+                }
+                //palletRequest(WS_Neighbour,currentPallet);
+                break;
+            }
+            default:{
+                res.end("ERROR");
+            }
+        }
+        res.end();
+    });
+
+    app.post('/'+WS+'pallet', function (req,res) {
+        console.log("*********",WS,"************",req.body);
+        setPallet(req.body);
+        res.end();
+    });
+
+    app.post('/'+WS+'palletRemoved', function (req,res) {
+        var palletID = req.body.palletID_;
+        resetPallets(palletID);
+        res.end();
+    });
+
+    if (port!=4001){
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z1_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z2_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z3_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z4_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z5_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimROB'+WSnum+'/events/DrawStartExecution/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimROB'+WSnum+'/events/DrawEndExecution/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimROB'+WSnum+'/services/ChangePen'+this.capability_,{form:{destUrl:"http://localhost"}}, function(err,httpResponse,body){});
+    }
+    else{
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z1_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z2_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z3_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimCNV'+WSnum+'/events/Z5_Changed/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+        request.post('http://localhost:3000/RTU/SimROB'+WSnum+'/events/PaperLoaded/notifs',{form:{destUrl:"http://localhost:"+port+"/"+WS+"notifs"}}, function(err,httpResponse,body){});
+    }
+    app.listen(port, hostname, function(){
+        console.log(WS+`Server running at http://${hostname}:${port}/`);
+    });
 };
 
 module.exports = WS_Agent;
--- test.js
+++ test.js
@@ -1,5 +1,52 @@
 /**
- * Created by halder on 09-Apr-17.
+ * Created by halder on 15-Apr-17.
  */
-var data = 'SimCNV1';
-console.log(data.substr(6,2));
+var arr = [ {palletID_: 1492328321500,
+    frameType_: 2,
+    frameColor_: 'BLUE',
+    screenType_: 5,
+    screenColor_: 'GREEN',
+    keyType_: 9,
+    keyColor_: 'RED',
+    status_: 0,
+    path_:
+[ [ 'WS1' ],
+    [ 'WS4', 'WS8', 'WS11' ],
+    [ 'WS3', 'WS6', 'WS10' ],
+    [ 'WS2', 'WS5', 'WS9', 'WS12' ] ]}, {palletID_: 1492328321501,
+    frameType_: 2,
+    frameColor_: 'BLUE',
+    screenType_: 5,
+    screenColor_: 'GREEN',
+    keyType_: 10,
+    keyColor_: 'RED',
+    status_: 0,
+    path_:
+        [ [ 'WS1' ],
+            [ 'WS4', 'WS8', 'WS11' ],
+            [ 'WS3', 'WS6', 'WS10' ],
+            [ 'WS2', 'WS5', 'WS9', 'WS12' ] ]}, {palletID_: 1492328321502,
+    frameType_: 2,
+    frameColor_: 'BLUE',
+    screenType_: 5,
+    screenColor_: 'GREEN',
+    keyType_: 8,
+    keyColor_: 'RED',
+    status_: 0,
+    path_:
+        [ [ 'WS1' ],
+            [ 'WS4', 'WS8', 'WS11' ],
+            [ 'WS3', 'WS6', 'WS10' ],
+            [ 'WS2', 'WS5', 'WS9', 'WS12' ] ]}
+];
+
+function findPallet(palletID){
+    for(var i=0;i<arr.length;i++){
+        if(arr[i].palletID_ == palletID)
+        {
+            console.log(arr[i]);
+        }
+    }
+}
+
+findPallet(1492328321502);
